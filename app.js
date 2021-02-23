60771e2fb854c6df017b746f04375ad521ec4642
Jehadmohmmad <Jehad.mohmmad9@gmail.com>
2021-02-23 21:05:24 +0200

    new change

---
+++ app.js
@@ -0,0 +1,32 @@
+var name = "jehad";
+var age = "30";
+var favsports = "boxing";
+
+
+var nameUserInput = prompt("guss what  is the name plyer? ");
+var ageUserInput = prompt("guss his age") ;
+var favsportsUserInput =("he is boxing player");
+
+if(nameUserInput=== name){
+    console.log("jehad is boxing player since 2005");
+}else{
+    console.log("the name of player is not correct!");
+}
+
+if (ageUserInput=== age){
+    console.log("your correct");
+
+}else{
+    console.log("Thats is not correct !!");
+}
+
+if (favsportsUserInput=== boxing){
+    console.log("Boxing one of hardest sportnin the World");
+
+}else{
+    console.log("you miss the hardest game !!");
+}
+
+
+    
+
